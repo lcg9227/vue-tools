c6dff9087b6d3e98a1f7ea1a50c1986d3c515fb5
lcg9227 <364304134@qq.com>
2023-03-30 16:05:25 +0800

    update

--- pages/ucenter/ucenter.vue
+++ pages/ucenter/ucenter.vue
@@ -1,30 +1,38 @@
 <template>
 	<view class="center">
 		<view class="userInfo" @click.capture="toUserInfo">
-			<cloud-image width="150rpx" height="150rpx" v-if="hasLogin&&userInfo.avatar_file&&userInfo.avatar_file.url" :src="userInfo.avatar_file.url"></cloud-image>
-			
+			<cloud-image width="150rpx" height="150rpx" v-if="hasLogin && userInfo.avatar_file && userInfo.avatar_file.url" :src="userInfo.avatar_file.url"></cloud-image>
+
 			<view v-else class="defaultAvatarUrl">
 				<uni-icons color="#ffffff" size="50" type="person-filled" />
 			</view>
-			
+
 			<view class="logo-title">
-				<text class="uer-name" v-if="hasLogin">{{userInfo.nickname||userInfo.username||userInfo.mobile}}</text>
-				<text class="uer-name" v-else>{{$t('mine.notLogged')}}</text>
+				<text class="uer-name" v-if="hasLogin">{{ userInfo.nickname || userInfo.username || userInfo.mobile }}</text>
+				<text class="uer-name" v-else>{{ $t('mine.notLogged') }}</text>
 			</view>
 		</view>
 		<uni-grid class="grid" :column="4" :showBorder="false" :square="true">
-			<uni-grid-item class="item" v-for="(item,index) in gridList" @click.native="tapGrid(index)" :key="index">
+			<uni-grid-item class="item" v-for="(item, index) in gridList" @click.native="tapGrid(index)" :key="index">
 				<uni-icons class="icon" color="#007AFF" :type="item.icon" size="26"></uni-icons>
-				<text class="text">{{item.text}}</text>
+				<text class="text">{{ item.text }}</text>
 			</uni-grid-item>
 		</uni-grid>
-		<uni-list class="center-list" v-for="(sublist , index) in ucenterList" :key="index">
-			<uni-list-item v-for="(item,i) in sublist" :title="item.title" link :rightText="item.rightText" :key="i"
-				:clickable="true" :to="item.to" @click="ucenterListClick(item)" :show-extra-icon="true"
-				:extraIcon="{type:item.icon,color:'#999'}">
+		<uni-list class="center-list" v-for="(sublist, index) in ucenterList" :key="index">
+			<uni-list-item
+				v-for="(item, i) in sublist"
+				:title="item.title"
+				link
+				:rightText="item.rightText"
+				:key="i"
+				:clickable="true"
+				:to="item.to"
+				@click="ucenterListClick(item)"
+				:show-extra-icon="true"
+				:extraIcon="{ type: item.icon, color: '#999' }">
 				<template v-slot:footer>
 					<view v-if="item.showBadge" class="item-footer">
-						<text class="item-footer-text">{{item.rightText}}</text>
+						<text class="item-footer-text">{{ item.rightText }}</text>
 						<view class="item-footer-badge"></view>
 					</view>
 				</template>
@@ -34,82 +42,86 @@
 </template>
 
 <script>
-	import checkUpdate from '@/uni_modules/uni-upgrade-center-app/utils/check-update';
-	import callCheckVersion from '@/uni_modules/uni-upgrade-center-app/utils/call-check-version';
+	import checkUpdate from '@/uni_modules/uni-upgrade-center-app/utils/check-update'
+	import callCheckVersion from '@/uni_modules/uni-upgrade-center-app/utils/call-check-version'
 	// #ifdef APP
-	import UniShare from '@/uni_modules/uni-share/js_sdk/uni-share.js';
+	import UniShare from '@/uni_modules/uni-share/js_sdk/uni-share.js'
 	const uniShare = new UniShare()
 	// #endif
-	const db = uniCloud.database();
-	import {
-		store,
-		mutations
-	} from '@/uni_modules/uni-id-pages/common/store.js'
+	const db = uniCloud.database()
+	import { store, mutations } from '@/uni_modules/uni-id-pages/common/store.js'
 	export default {
 		// #ifdef APP
-		onBackPress({from}) {
-			if(from=='backbutton'){
-				this.$nextTick(function(){
+		onBackPress({ from }) {
+			if (from == 'backbutton') {
+				this.$nextTick(function () {
 					uniShare.hide()
 				})
-				return uniShare.isShow;
+				return uniShare.isShow
 			}
 		},
 		// #endif
 		data() {
 			return {
-				gridList: [{
-						"text": this.$t('mine.showText'),
-						"icon": "chat"
-					},
-					{
-						"text": this.$t('mine.showText'),
-						"icon": "cloud-upload"
-					},
-					{
-						"text": this.$t('mine.showText'),
-						"icon": "contact"
-					},
-					{
-						"text": this.$t('mine.showText'),
-						"icon": "download"
-					}
+				gridList: [
+					// {
+					// 	text: this.$t('mine.showText'),
+					// 	icon: 'chat',
+					// },
+					// {
+					// 	text: this.$t('mine.showText'),
+					// 	icon: 'cloud-upload',
+					// },
+					// {
+					// 	text: this.$t('mine.showText'),
+					// 	icon: 'contact',
+					// },
+					// {
+					// 	text: this.$t('mine.showText'),
+					// 	icon: 'download',
+					// },
 				],
 				ucenterList: [
 					[
 						{
-							"title":this.$t('mine.readArticles'),
-							"to": '/pages/ucenter/read-news-log/read-news-log',
-							"icon": "flag"
+							title: this.$t('mine.readArticles'),
+							to: '/pages/ucenter/read-news-log/read-news-log',
+							icon: 'flag',
 						},
 					],
-					[{
-						"title": this.$t('mine.feedback'),
-						"to": '/uni_modules/uni-feedback/pages/opendb-feedback/opendb-feedback',
-						"icon": "help"
-					}, {
-						"title": this.$t('mine.settings'),
-						"to": '/pages/ucenter/settings/settings',
-						"icon": "gear"
-					}],
+					[
+						{
+							title: this.$t('mine.feedback'),
+							to: '/uni_modules/uni-feedback/pages/opendb-feedback/opendb-feedback',
+							icon: 'help',
+						},
+						{
+							title: this.$t('mine.settings'),
+							to: '/pages/ucenter/settings/settings',
+							icon: 'gear',
+						},
+					],
 					// #ifdef APP-PLUS
-					[{
-						"title": this.$t('mine.about'),
-						"to": '/pages/ucenter/about/about',
-						"icon": "info"
-					}]
+					[
+						{
+							title: this.$t('mine.about'),
+							to: '/pages/ucenter/about/about',
+							icon: 'info',
+						},
+					],
 					// #endif
 				],
 				listStyles: {
-					"height": "150rpx", // 边框高度
-					"width": "150rpx", // 边框宽度
-					"border": { // 如果为 Boolean 值，可以控制边框显示与否
-						"color": "#eee", // 边框颜色
-						"width": "1px", // 边框宽度
-						"style": "solid", // 边框样式
-						"radius": "100%" // 边框圆角，支持百分比
-					}
-				}
+					height: '150rpx', // 边框高度
+					width: '150rpx', // 边框宽度
+					border: {
+						// 如果为 Boolean 值，可以控制边框显示与否
+						color: '#eee', // 边框颜色
+						width: '1px', // 边框宽度
+						style: 'solid', // 边框样式
+						radius: '100%', // 边框圆角，支持百分比
+					},
+				},
 			}
 		},
 		onLoad() {
@@ -123,13 +135,12 @@
 			// })
 			//#endif
 		},
-		onShow() {
-		},
+		onShow() {},
 		computed: {
 			userInfo() {
 				return store.userInfo
 			},
-			hasLogin(){
+			hasLogin() {
 				return store.hasLogin
 			},
 			// #ifdef APP-PLUS
@@ -139,12 +150,12 @@
 			// #endif
 			appConfig() {
 				return getApp().globalData.config
-			}
+			},
 		},
 		methods: {
 			toSettings() {
 				uni.navigateTo({
-					url: "/pages/ucenter/settings/settings"
+					url: '/pages/ucenter/settings/settings',
 				})
 			},
 			/**
@@ -152,34 +163,34 @@
 			 */
 			ucenterListClick(item) {
 				if (!item.to && item.event) {
-					this[item.event]();
+					this[item.event]()
 				}
 			},
 			async checkVersion() {
 				let res = await callCheckVersion()
-				console.log(res);
+				console.log(res)
 				if (res.result.code > 0) {
 					checkUpdate()
 				} else {
 					uni.showToast({
 						title: res.result.message,
-						icon: 'none'
-					});
+						icon: 'none',
+					})
 				}
 			},
 			toUserInfo() {
 				uni.navigateTo({
-					url: '/uni_modules/uni-id-pages/pages/userinfo/userinfo'
+					url: '/uni_modules/uni-id-pages/pages/userinfo/userinfo',
 				})
 			},
 			tapGrid(index) {
 				uni.showToast({
 					// title: '你点击了，第' + (index + 1) + '个',
-					title: this.$t('mine.clicked') + " " + (index + 1) ,
-					icon: 'none'
-				});
+					title: this.$t('mine.clicked') + ' ' + (index + 1),
+					icon: 'none',
+				})
 			},
-		}
+		},
 	}
 </script>
 
@@ -195,7 +206,7 @@
 		background-color: #f8f8f8;
 	}
 	/* #endif*/
-	
+
 	.center {
 		flex: 1;
 		flex-direction: column;
@@ -209,7 +220,7 @@
 		flex-direction: column;
 		align-items: center;
 	}
-	.defaultAvatarUrl{
+	.defaultAvatarUrl {
 		width: 150rpx;
 		height: 150rpx;
 		background-color: #007aff;
@@ -229,7 +240,7 @@
 		height: 100rpx;
 		line-height: 100rpx;
 		font-size: 38rpx;
-		color: #FFFFFF;
+		color: #ffffff;
 	}
 
 	.center-list {
@@ -239,12 +250,12 @@
 
 	.center-list-cell {
 		width: 750rpx;
-		background-color: #007AFF;
+		background-color: #007aff;
 		height: 40rpx;
 	}
 
 	.grid {
-		background-color: #FFFFFF;
+		background-color: #ffffff;
 		margin-bottom: 6px;
 	}
 
@@ -259,7 +270,6 @@
 		justify-content: center;
 		align-items: center;
 	}
-
 
 	/*修改边线粗细示例*/
 	/* #ifndef APP-NVUE */
@@ -295,6 +305,6 @@
 		/* #ifdef APP-NVUE */
 		border-radius: 10rpx;
 		/* #endif */
-		background-color: #DD524D;
+		background-color: #dd524d;
 	}
 </style>
