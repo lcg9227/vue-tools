ad982aa47af8cda7b722daf35140fc5822866dc7
lcg9227 <364304134@qq.com>
2023-03-30 16:41:23 +0800

    update

--- App.vue
+++ App.vue
@@ -1,6 +1,5 @@
 <script>
 	import initApp from '@/common/appInit.js';
-	import checkIsAgree from '@/pages/uni-agree/utils/uni-agree.js';
 	import uniIdPageInit from '@/uni_modules/uni-id-pages/init.js';
 	export default {
 		globalData: {
@@ -16,15 +15,6 @@
 			this.globalData.$t = str => this.$t(str)
 			initApp();
 			uniIdPageInit()
-			
-			// #ifdef APP-PLUS
-			//checkIsAgree(); APP端暂时先用原生默认生成的。目前，自定义方式启动vue界面时，原生层已经请求了部分权限这并不符合国家的法规
-			// #endif
-
-			// #ifdef H5
-			// checkIsAgree(); // 默认不开启。目前全球，仅欧盟国家有网页端同意隐私权限的需要。如果需要可以自己去掉注视后生效
-			// #endif
-
 			// #ifdef APP-PLUS
 			//idfa有需要的用户在应用首次启动时自己获取存储到storage中
 			/*var idfa = '';
