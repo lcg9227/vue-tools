d1215147ad522e3d6c9be91916b2f6508e6818f2
lcg9227 <364304134@qq.com>
2023-06-02 15:42:06 +0800

    update

---
+++ uniCloud-aliyun/cloudfunctions/common/lcg-common/index.js
@@ -0,0 +1,28 @@
+const db = uniCloud.database()
+const dbCmd = db.command
+const usersTable = db.collection('uni-id-users')
+
+/* 判断是否是家长账号 */
+const isParent = async function (userInfo) {
+	const ret = {
+		success: true,
+		errMsg: ''
+	}
+	// 查询家长账号
+	const parentTable = usersTable.where({ _id: userInfo._id }).limit(1)
+	const { data: parents } = await parentTable.get()
+	const parent = parents[0]
+	let { role } = parent
+	if (!role.includes('parent')) {
+		ret.success = false
+		ret.errMsg = '不是家长账号！'
+		return ret
+	}
+	ret.data = parent
+	ret.parentTable = parentTable
+	return ret
+}
+
+module.exports = {
+	isParent
+}
