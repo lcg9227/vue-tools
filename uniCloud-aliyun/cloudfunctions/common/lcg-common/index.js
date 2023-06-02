7469d778bbf0c8f35f5efd93480761f076ff0bd6
lcg9227 <364304134@qq.com>
2023-06-02 16:12:06 +0800

    update

--- uniCloud-aliyun/cloudfunctions/common/lcg-common/index.js
+++ uniCloud-aliyun/cloudfunctions/common/lcg-common/index.js
@@ -3,13 +3,13 @@ const dbCmd = db.command
 const usersTable = db.collection('uni-id-users')
 
 /* 获取账号信息 */
-const getUserInfo = async function (userInfo) {
+const getUserInfo = async function (_id) {
 	const ret = {
 		success: true,
 		errMsg: ''
 	}
 	// 查询账号信息
-	const userTable = usersTable.where({ _id: userInfo._id }).limit(1)
+	const userTable = usersTable.where({ _id }).limit(1)
 	const { data: users } = await userTable.get()
 	const user = users[0]
 	let { role } = user
