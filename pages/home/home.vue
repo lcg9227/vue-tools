785ca56bcfc671b1f94747b6c3190f4256f6d1eb
lcg9227 <364304134@qq.com>
2023-06-06 08:30:11 +0800

    update

---
+++ pages/home/home.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="warp" v-if="!loading">
+		<lcg-nav-bar :statusBarStyle="{ backgroundColor: '#69c98b' }"></lcg-nav-bar>
+		<lcg-header :userDetail="userDetail" :config="config"></lcg-header>
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {},
+		data() {
+			return {
+				loading: true,
+				current: 0,
+				userInfo: {},
+				userDetail: {},
+				config: {}
+			}
+		},
+		created() {
+			this.userInfo = this.api.getUserInfo()
+			this.getData()
+		},
+		methods: {
+			getData() {
+				const { hasLogin } = this.userInfo
+				if (!hasLogin) return
+				this.getConfig()
+					.then(() => this.getDetail())
+					.then(() => (this.loading = false))
+			},
+			getConfig() {
+				return this.api.get_config().then(config => (this.config = config))
+			},
+			getDetail() {
+				const { username } = this.userInfo
+				return this.api.get_user_detail(username).then(({ data: userDetail }) => {
+					this.userDetail = userDetail
+					console.log('userDetail >>>', username, this.config, userDetail)
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.warp {
+		position: relative;
+	}
+</style>
