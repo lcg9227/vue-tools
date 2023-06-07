62fb5567b9a8a340c05df241135714948f5e3b76
lcg9227 <364304134@qq.com>
2023-06-07 13:44:41 +0800

    update

--- components/lcg-task-form/lcg-task-form.vue
+++ components/lcg-task-form/lcg-task-form.vue
@@ -1,37 +1,39 @@
 <template>
 	<uni-popup ref="popup" type="top">
 		<div class="popup" :style="style">
-			<div class="content" v-if="showForm">
-				<uni-forms :model="fields" ref="form" :label-width="100" :rules="rules">
-					<uni-forms-item label="任务类型" name="task_type" required>
-						<uni-data-checkbox v-model="fields.task_type" :localdata="localdata.task_type"></uni-data-checkbox>
-					</uni-forms-item>
-					<uni-forms-item label="任务名称" name="name" required>
-						<uni-easyinput type="text" v-model="fields.name" placeholder="请输入任务名称" :maxlength="20" />
-					</uni-forms-item>
-					<uni-forms-item label="图标选择" name="icon" required>
-						<lcg-select v-model="fields.icon" :localdata="localdata.icon"></lcg-select>
-					</uni-forms-item>
-					<uni-forms-item label="图标颜色选择" name="icon_color" required v-if="fields.icon.includes('t-icon-')">
-						<lcg-select v-model="fields.icon_color" :localdata="localdata.icon_color"></lcg-select>
-					</uni-forms-item>
-					<uni-forms-item label="执行类型" name="execute_type" required>
-						<uni-data-checkbox v-model="fields.execute_type" :localdata="localdata.execute_type"></uni-data-checkbox>
-					</uni-forms-item>
-					<uni-forms-item label="执行天数" name="execute_days" required v-if="fields.execute_type === 1">
-						<uni-easyinput type="number" v-model="fields.execute_days" placeholder="请输入执行天数" :maxlength="3" />
-					</uni-forms-item>
-					<uni-forms-item label="执行周选择" name="execute_weeks" required v-if="fields.execute_type === 2">
-						<uni-data-checkbox multiple v-model="fields.execute_weeks" :localdata="localdata.execute_weeks"></uni-data-checkbox>
-					</uni-forms-item>
-					<uni-forms-item label="奖励积分" name="reward" required>
-						<uni-easyinput type="number" v-model="fields.reward" placeholder="请输入当前任务的奖励积分" :maxlength="10" />
-					</uni-forms-item>
-					<uni-forms-item label="任务说明" name="describe" required>
-						<uni-easyinput type="textarea" v-model="fields.describe" placeholder="请输入任务说明" :maxlength="200" />
-					</uni-forms-item>
-				</uni-forms>
-			</div>
+			<scroll-view :scroll-y="true" :style="{ height: scrollHeight }">
+				<div class="content" v-if="showForm">
+					<uni-forms :model="fields" ref="form" :label-width="100" :rules="rules">
+						<uni-forms-item label="任务类型" name="task_type" required>
+							<uni-data-checkbox v-model="fields.task_type" :localdata="localdata.task_type"></uni-data-checkbox>
+						</uni-forms-item>
+						<uni-forms-item label="任务名称" name="name" required>
+							<uni-easyinput type="text" v-model="fields.name" placeholder="请输入任务名称" :maxlength="20" />
+						</uni-forms-item>
+						<uni-forms-item label="图标选择" name="icon" required>
+							<lcg-select v-model="fields.icon" :localdata="localdata.icon"></lcg-select>
+						</uni-forms-item>
+						<uni-forms-item label="图标颜色选择" name="icon_color" required v-if="fields.icon.includes('t-icon-')">
+							<lcg-select v-model="fields.icon_color" :localdata="localdata.icon_color"></lcg-select>
+						</uni-forms-item>
+						<uni-forms-item label="执行类型" name="execute_type" required>
+							<uni-data-checkbox v-model="fields.execute_type" :localdata="localdata.execute_type"></uni-data-checkbox>
+						</uni-forms-item>
+						<uni-forms-item label="执行天数" name="execute_days" required v-if="fields.execute_type === 1">
+							<uni-easyinput type="number" v-model="fields.execute_days" placeholder="请输入执行天数" :maxlength="3" />
+						</uni-forms-item>
+						<uni-forms-item label="执行周选择" name="execute_weeks" required v-if="fields.execute_type === 2">
+							<uni-data-checkbox multiple v-model="fields.execute_weeks" :localdata="localdata.execute_weeks"></uni-data-checkbox>
+						</uni-forms-item>
+						<uni-forms-item label="奖励积分" name="reward" required>
+							<uni-easyinput type="number" v-model="fields.reward" placeholder="请输入当前任务的奖励积分" :maxlength="10" />
+						</uni-forms-item>
+						<uni-forms-item label="任务说明" name="describe" required>
+							<uni-easyinput type="textarea" v-model="fields.describe" placeholder="请输入任务说明" :maxlength="200" />
+						</uni-forms-item>
+					</uni-forms>
+				</div>
+			</scroll-view>
 			<div class="footer">
 				<button class="closeButton" type="warn" @click="onClose">取消</button>
 				<button class="submitButton" type="primary" @click="onSubmit">添加</button>
@@ -51,10 +53,12 @@
 			const { proxy } = getCurrentInstance()
 			// 弹窗样式设置
 			const winInfo = uni.getWindowInfo()
-			const height = `${winInfo.screenHeight - 80}px`
+			const height = `${winInfo.windowHeight - 20}px`
 			const paddingTop = `${winInfo.statusBarHeight + 20}px`
+			const scrollHeight = `${winInfo.windowHeight - 80}px`
 			const style = reactive({
 				height,
+				maxHeight: height,
 				paddingTop
 			})
 			// 标题
@@ -95,7 +99,7 @@
 					showForm.value = true
 				})
 			}
-			return { style, showForm, fields, rules, localdata, title, onSubmit, onClose, open }
+			return { style, scrollHeight, showForm, fields, rules, localdata, title, onSubmit, onClose, open }
 		}
 	}
 </script>
@@ -115,9 +119,11 @@
 			display: flex;
 			flex-direction: row;
 			width: 100%;
-			padding: 30rpx;
+			height: 70px;
+			align-items: center;
 			box-sizing: border-box;
 			box-shadow: 0 -2rpx 20rpx #f2f2f2;
+			background-color: #fff;
 			.submitButton {
 				border-radius: 100rpx;
 				background-color: $lcg-main-button-bg;
